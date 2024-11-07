bf3bfa2bf66292914d95e639444042cad64671f2
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-11-07 14:12:58 -0300

    feat: Add  route to handle product-related API requests

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js";
+
 import connectMongoDB from "./lib/db.js";
 
 dotenv.config();
@@ -12,6 +14,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json()); // alows to parse the body of the request
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 
 app.listen(5000, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
